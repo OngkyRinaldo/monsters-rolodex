407ddf9687a21163698663c4567627174f91dcf7
OngkyRinaldo <edoorinaldoo@gmail.com>
2023-07-23 20:41:45 +0700

    update layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import SearchBox from './components/SearchBox';
 
 const App = () => {
     return (
-        <main className='bg-gradient-to-l from-rose-100 to-teal-100 w-screen  font-Bigelow pb-10 '>
+        <main className='bg-gradient-to-l from-rose-100 to-teal-100 w-screen min-h-screen  font-Bigelow pb-10 '>
             <h1 className='text-center text-5xl pt-20 mb-12 text-main'>
                 Monsters Rolodex
             </h1>
--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -7,7 +7,7 @@ const CardList = () => {
     const { filteredMonsters } = useContext(DataContext);
 
     return (
-        <div className='container mx-auto  grid grid-cols-2 lg:grid-cols-4 gap-4 h-screen'>
+        <div className='container mx-auto  grid grid-cols-2 lg:grid-cols-4 gap-4 '>
             {filteredMonsters.map((monster) => {
                 return <Card monster={monster} key={monster.id} />;
             })}
